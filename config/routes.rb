e9db4cc44a3f2244d16f9fc0bf965c50fb32b7a6
Daisy Yi <daisyyi@rocketmail.com>
2023-06-27 11:35:35 -0700

    add route to photographers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # get "/photographers", to: "photographers#index"
   # get "/photographers/:id", to: "photographer#show"
   # get "/photos", to: "photos#index"
-  resources :photographers, only: [:index, :show, :create]
+  resources :photographers
   resources :exhibits, only: [:index, :show]
   resources :photos
   # get "/exhibits", to: "exhibits#index"

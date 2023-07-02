59140deaa92c1d4840c165191ecbf899834c64cf
Daisy Yi <daisyyi@rocketmail.com>
2023-07-02 14:17:54 -0700

    add nested route to photographers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # get "/photographers", to: "photographers#index"
   # get "/photographers/:id", to: "photographer#show"
   # get "/photos", to: "photos#index"
-  resources :photographers
+  resources :photographers do
+    resources :photos, only [:show, :index]
+  end
+  
   resources :exhibits, only: [:index, :show]
   resources :photos
   # get "/exhibits", to: "exhibits#index"

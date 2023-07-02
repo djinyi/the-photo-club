94e5b60a385a5dd59ac67fdfa22b29cb9b26002d
Daisy Yi <daisyyi@rocketmail.com>
2023-07-02 14:28:47 -0700

    add column to photos

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # get "/photographers/:id", to: "photographer#show"
   # get "/photos", to: "photos#index"
   resources :photographers do
-    resources :photos, only [:show, :index]
+    resources :photos, only: [:show, :index]
   end
-  
+
   resources :exhibits, only: [:index, :show]
   resources :photos
   # get "/exhibits", to: "exhibits#index"
---
+++ db/migrate/20230702212818_add_photographer_id_to_photos.rb
@@ -0,0 +1,4 @@
+class AddPhotographerIdToPhotos < ActiveRecord::Migration[6.1]
+  def change
+  end
+end

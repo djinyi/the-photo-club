a5c9e975bd29ac25d001ec0f4987bc206af62840
Daisy Yi <daisyyi@rocketmail.com>
2023-07-06 13:34:54 -0700

    add belongs_to

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -2,4 +2,5 @@ class PhotoSerializer < ActiveModel::Serializer
   attributes :id, :image_url, :title, :year, :description, :medium, :photographer_id, :exhibit_id
 
   belongs_to :photographer
+  belongs_to :exhibit
 end

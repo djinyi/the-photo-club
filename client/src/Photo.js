a71e78136001c85f276390638df7fd5843b530db
Daisy Yi <daisyyi@rocketmail.com>
2023-07-12 20:30:23 -0700

    add other photo attributes etc

--- client/src/Photo.js
+++ client/src/Photo.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Photo({ title, url, id }){
+function Photo({ title, url, id , description, year, medium }){
     return(
         <div>
             <p>title: {title}</p>
             <img src={url} width="500" />
+            <p>medium: {medium}</p>
+            <p>year: {year}</p>
+            <p>description: {description}</p>
         </div>
     )
 }

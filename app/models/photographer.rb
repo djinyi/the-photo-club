b65da88291fe9fbbd8b4c8ecb1bb780273fdc675
Daisy Yi <daisyyi@rocketmail.com>
2023-07-12 23:30:24 -0700

    update user model

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,6 +1,7 @@
 class Photographer < ApplicationRecord
     has_many :photos, dependent: :destroy
     has_many :exhibits, through: :photos, dependent: :destroy
+    has_one :user
     validates :name, presence: true, length: {maximum: 15}
 
 end

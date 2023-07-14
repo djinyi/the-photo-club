2642d48c64aa3c5bd8725d70bc0ac8632f24203a
Daisy Yi <daisyyi@rocketmail.com>
2023-07-13 21:29:52 -0700

    edit authorize method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
     end
 
     def authorize
-        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include?(:user_id)
     end
       
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,6 @@ class UsersController < ApplicationController
     end
 
     def authorize
-        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include?(:user_id)
     end
 end

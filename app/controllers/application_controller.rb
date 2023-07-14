181c670769d5dd579fa38c6d2db2a71ca7e8a7ff
Daisy Yi <daisyyi@rocketmail.com>
2023-07-13 21:26:13 -0700

    add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::API
 
   before_action :set_current_user
 
-
+  def set_current_user
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 
 end

062688e513cd777ed529564b5618561455c01c1d
Daisy Yi <daisyyi@rocketmail.com>
2023-07-18 10:57:47 -0700

    add fetch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
-  before_action :set_current_user
-  # before_action :authorize
+  # before_action :set_current_user
+  # # before_action :authorize
 
-  def set_current_user
-    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
-  end
+  # def set_current_user
+  #   Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  # end
 
 
   # def authorize

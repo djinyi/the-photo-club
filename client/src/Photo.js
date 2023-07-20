3396f3455d3a0167e7477b3b2a6aefede7a6a4ce
Daisy Yi <daisyyi@rocketmail.com>
2023-07-20 12:52:48 -0700

    got delete to work

--- client/src/Photo.js
+++ client/src/Photo.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Photo({ title, url, id , description, year, medium }){
+function Photo({ title, url, id , description, year, medium, onDeletePost}){
+    const [titled, setTitled] = useState(title)
+    const [descriptiond, setDescriptiond] = useState(description)
+    const [yeard, setYeard] = useState(year)
+    const [mediumd, setMediumd] = useState(medium)
+
+    function handleDeleteClick() {
+        fetch(`/photos/${id}`, {
+            method: "DELETE",
+        })
+        .then((r) => r.json())
+        .then((post) => onDeletePost(post))
+     }
+
     return(
         <div>
             <p>title: {title}</p>
@@ -10,6 +23,7 @@ function Photo({ title, url, id , description, year, medium }){
             <p>year: {year}</p>
             <p>description: {description}</p>
             <Link to={`/photos/${id}`}>Edit</Link>
+            <button onClick={handleDeleteClick}> Delete Post</button>
         </div>
     )
 }

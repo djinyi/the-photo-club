0b3c6c2f663c340f4b76bccd12d693707e88a6a2
Daisy Yi <daisyyi@rocketmail.com>
2023-07-21 11:26:59 -0700

    add UserProvider

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from "react-router-dom"
 import App from './App';
+import { UserProvider } from "../context/user";
 
 ReactDOM.render(
   <BrowserRouter>
+    <UserProvider>
       <App />
+    </UserProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );

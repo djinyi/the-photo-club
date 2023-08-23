487a838bb6a07da228409e3dd03123726f5312cf
Daisy Yi <daisyyi@rocketmail.com>
2023-08-23 15:13:20 -0700

    clean code

--- client/src/user/UserContext.js
+++ client/src/user/UserContext.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 
-// create the context
 const UserContext = React.createContext();
 
-// create a provider component
 function UserProvider({ children }) {
     const [user, setUser] = useState(null);
 
@@ -14,8 +12,7 @@ function UserProvider({ children }) {
           }
         });
       }, []);
-  // the value prop of the provider will be our context data
-  // this value will be available to child components of this provider
+
   return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>;
 }
 
